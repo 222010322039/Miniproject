581ffb85645bec638e5e200ae326ab0d9a192d98
222010322039 <86588207+222010322039@users.noreply.github.com>
2023-10-26 18:31:12 +0530

    Update ala.py

--- ala.py
+++ ala.py
@@ -1,58 +1,34 @@
-
 import streamlit as st
 import pdfplumber
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
 
 st.title("Candidate Selection Tool")
 st.subheader("NLP Based Resume Screening")
 
-st.caption("Aim of this project is to check whether a candidate is qualified for a role based on his or her education, experience, and other information captured on their resume. In a nutshell, it's a form of pattern matching between a job's requirements and the qualifications of a candidate based on their resume.")
-
-uploadedJD = st.file_uploader("Upload Job Description", type="pdf")
-uploadedResumes = st.file_uploader("Upload resumes", type="pdf", accept_multiple_files=True)
-
-click = st.button("Process")
+# ... (Rest of your code)
 
 if click and uploadedJD and uploadedResumes:
-    try:
-        with pdfplumber.open(uploadedJD) as pdf:
-            job_description = pdf.pages[0].extract_text()
-    except:
-        st.write("Error reading job description PDF")
-
-    job_description = job_description.lower()
-
-    st.write("Job Description:")
-    st.write(job_description)
-
-    cv = CountVectorizer()
-    matrix = cv.fit_transform([job_description])
-
-    matches = []
-
-    for idx, uploadedResume in enumerate(uploadedResumes):
-        try:
-            with pdfplumber.open(uploadedResume) as pdf:
-                resume_text = pdf.pages[0].extract_text()
-        except:
-            st.write(f"Error reading resume {idx + 1} PDF")
-
-        resume_text = resume_text.lower()
-
-        similarity_matrix = cosine_similarity(matrix, cv.transform([resume_text]))
-        match = similarity_matrix[0][0] * 100
-        match = round(match, 2)
-
-        matches.append((match, resume_text))
+    # ... (Your existing code)
 
     matches.sort(key=lambda x: x[0], reverse=True)
 
     st.write("Top Resumes:")
+    percentages = [match[0] for match in matches]  # Extract match percentages
+
     for i in range(len(matches)):
         match_percentage, resume_text = matches[i]
         st.write(f"Match Percentage for Resume {i + 1}: {match_percentage}%")
         st.write(resume_text)
         st.write("-" * 50)
+
+    # Create a bar graph
+    plt.figure(figsize=(10, 6))
+    plt.bar([f"Resume {i+1}" for i in range(len(percentages))], percentages)
+    plt.xlabel("Resumes")
+    plt.ylabel("Match Percentage")
+    plt.title("Match Percentage for Resumes")
+    st.pyplot(plt)  # Display the graph in Streamlit
 
 st.caption(" ~ made by Team P7132")
