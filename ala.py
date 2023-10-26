6a673f7995d041d744fc7be6af1f701679382c61
222010322039 <86588207+222010322039@users.noreply.github.com>
2023-10-26 19:35:23 +0530

    Update ala.py

--- ala.py
+++ ala.py
@@ -4,7 +4,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import altair as alt
-import re  # Import the regular expressions module
 
 st.title("Candidate Selection Tool")
 st.subheader("NLP Based Resume Screening")
@@ -36,8 +35,6 @@ if click and uploadedJD and uploadedResumes:
 
     matches = []
     skills_count = []
-    btech_cgpa = []
-    academic_percentages = []
 
     for idx, uploadedResume in enumerate(uploadedResumes):
         try:
@@ -58,20 +55,6 @@ if click and uploadedJD and uploadedResumes:
         skill_count = {skill: resume_text.count(skill.lower()) for skill in skills_to_search}
         skills_count.append(skill_count)
 
-        # Extract B.Tech CGPA and academic percentages using regular expressions
-        cgpa_match = re.search(r"B\.Tech CGPA: (\d+\.\d+|\d+)", resume_text)
-        academic_cgpa_match = re.search(r"CGPA: (\d+\.\d+|\d+)%", resume_text)
-
-        if cgpa_match:
-            btech_cgpa.append(float(cgpa_match.group(1)))
-        else:
-            btech_cgpa.append(None)
-
-        if academic_cgpa_match:
-            academic_percentages.append(float(academic_cgpa_match.group(1)))
-        else:
-            academic_percentages.append(None)
-
     matches.sort(key=lambda x: x[0], reverse=True)
 
     st.write("Top Resumes:")
@@ -80,38 +63,12 @@ if click and uploadedJD and uploadedResumes:
     for i in range(len(matches)):
         match_percentage, resume_text = matches[i]
         st.write(f"Match Percentage for Resume {i + 1}: {match_percentage}%")
-
-        # Extract and display a shorter description (e.g., first 5 lines)
-        resume_lines = resume_text.split('\n')[:5]
-        summarized_resume = "\n".join(resume_lines)
-        st.write("Summary of Resume:")
-        st.write(summarized_resume)
-
+        st.write(resume_text)
         st.write("Skills Count:")
         st.write(skills_count[i])
-
-        # Display B.Tech CGPA
-        st.write("B.Tech CGPA: {:.2f}".format(btech_cgpa[i] if btech_cgpa[i] is not None else 0.0))
-
-        # Display academic percentage
-        st.write("Academic Percentage: {:.2f}%".format(academic_percentages[i] if academic_percentages[i] is not None else 0.0))
-
         st.write("-" * 50)
 
-    # Create a bar chart using Altair for B.Tech CGPA
-    cgpa_df = pd.DataFrame({'Resume': [f"Resume {i + 1}" for i in range(len(btech_cgpa))], 'B.Tech CGPA': btech_cgpa})
-    
-    cgpa_chart = alt.Chart(cgpa_df).mark_bar().encode(
-        x='Resume:N',
-        y='B.Tech CGPA:Q',
-        color='Resume:N'
-    ).properties(
-        width=600  # Increase the width as needed
-    )
-
-    st.altair_chart(cgpa_chart, use_container_width=True)
-
-    # Create a bar chart for skills count
+    # Create a bar chart using Altair for skills count
     skills_df = pd.DataFrame(skills_count)
     skills_df['Resume'] = [f"Resume {i+1}" for i in range(len(skills_count))]
 
@@ -130,7 +87,7 @@ if click and uploadedJD and uploadedResumes:
 
     # Create a bar chart for match percentages
     match_percentages_df = pd.DataFrame({
-        'Resume': [f"Resume {i + 1}" for i in range(len(percentages))],
+        'Resume': [f"Resume {i+1}" for i in range(len(percentages))],
         'Match Percentage': percentages
     })
 
@@ -143,5 +100,4 @@ if click and uploadedJD and uploadedResumes:
 
     st.altair_chart(match_percentages_chart, use_container_width=True)
 
-st.caption(" ~ made by Team P7132")
-
+st.caption(" ~ made by Team P7132")
