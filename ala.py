38bff2e51bfdde3b38efb56a4e648d05f4ed6598
222010322039 <86588207+222010322039@users.noreply.github.com>
2023-10-26 19:14:34 +0530

    Update ala.py

--- ala.py
+++ ala.py
@@ -73,7 +73,7 @@ if click and uploadedJD and uploadedResumes:
 
     # Create a bar chart using Altair for skills count
     skills_df = pd.DataFrame(skills_count)
-    skills_df['Resume'] = [f"Resume {i+1}" for i in range(len(skills_count))]
+    skills_df['Resume'] = [f"Resume {i+1}" for i in range(len(percentages))]
 
     skills_chart = alt.Chart(skills_df).transform_fold(
         skills_to_search,
@@ -90,7 +90,7 @@ if click and uploadedJD and uploadedResumes:
 
     # Create a bar chart for match percentages
     match_percentages_df = pd.DataFrame({
-        'Resume': [f"Resume {i+1}" for i in range(len(percentages)],
+        'Resume': [f"Resume {i+1}" for i in range(len(percentages))],
         'Match Percentage': percentages
     })
 
