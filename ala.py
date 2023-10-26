5a519fdb0af5199694b3b78874af96e76ab6b0b7
222010322039 <86588207+222010322039@users.noreply.github.com>
2023-10-26 18:35:10 +0530

    Update ala.py

--- ala.py
+++ ala.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pdfplumber
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import matplotlib_inline as mpli  # Import matplotlib-inline
+import pandas as pd
+import altair as alt
 
 st.title("Candidate Selection Tool")
 st.subheader("NLP Based Resume Screening")
@@ -10,7 +11,36 @@ st.subheader("NLP Based Resume Screening")
 # ... (Rest of your code)
 
 if click and uploadedJD and uploadedResumes:
-    # ... (Your existing code)
+    try:
+        with pdfplumber.open(uploadedJD) as pdf:
+            job_description = pdf.pages[0].extract_text()
+    except:
+        st.write("Error reading job description PDF")
+
+    job_description = job_description.lower()
+
+    st.write("Job Description:")
+    st.write(job_description)
+
+    cv = CountVectorizer()
+    matrix = cv.fit_transform([job_description])
+
+    matches = []
+
+    for idx, uploadedResume in enumerate(uploadedResumes):
+        try:
+            with pdfplumber.open(uploadedResume) as pdf:
+                resume_text = pdf.pages[0].extract_text()
+        except:
+            st.write(f"Error reading resume {idx + 1} PDF")
+
+        resume_text = resume_text.lower()
+
+        similarity_matrix = cosine_similarity(matrix, cv.transform([resume_text]))
+        match = similarity_matrix[0][0] * 100
+        match = round(match, 2)
+
+        matches.append((match, resume_text))
 
     matches.sort(key=lambda x: x[0], reverse=True)
 
@@ -23,13 +53,15 @@ if click and uploadedJD and uploadedResumes:
         st.write(resume_text)
         st.write("-" * 50)
 
-    # Create a bar graph
-    plt.figure(figsize=(10, 6))
-    plt.bar([f"Resume {i+1}" for i in range(len(percentages))], percentages)
-    plt.xlabel("Resumes")
-    plt.ylabel("Match Percentage")
-    plt.title("Match Percentage for Resumes")
+    # Create a bar chart using Altair
+    df = pd.DataFrame({'Resume': [f"Resume {i+1}" for i in range(len(percentages)], 'Match Percentage': percentages})
+    chart = alt.Chart(df).mark_bar().encode(
+        x='Resume',
+        y='Match Percentage'
+    ).properties(
+        width=400
+    )
 
-    st.pyplot(plt)  # Display the graph in Streamlit using st.pyplot
+    st.altair_chart(chart, use_container_width=True)
 
 st.caption(" ~ made by Team P7132")
