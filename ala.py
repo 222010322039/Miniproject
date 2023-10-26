0097202cec933804ede87469fcf00aa5dc53abb6
222010322039 <86588207+222010322039@users.noreply.github.com>
2023-10-26 18:26:54 +0530

    Update ala.py

--- ala.py
+++ ala.py
@@ -1,9 +1,8 @@
+
 import streamlit as st
 import pdfplumber
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import gensim
-from gensim.summarization import summarize
 
 st.title("Candidate Selection Tool")
 st.subheader("NLP Based Resume Screening")
@@ -45,23 +44,15 @@ if click and uploadedJD and uploadedResumes:
         match = similarity_matrix[0][0] * 100
         match = round(match, 2)
 
-        # Summarize the resume text
-        summarized_resume = summarize(resume_text, ratio=0.2)  # Adjust ratio as needed
-
-        matches.append((match, summarized_resume))
+        matches.append((match, resume_text))
 
     matches.sort(key=lambda x: x[0], reverse=True)
 
     st.write("Top Resumes:")
     for i in range(len(matches)):
-        match_percentage, summarized_resume = matches[i]
+        match_percentage, resume_text = matches[i]
         st.write(f"Match Percentage for Resume {i + 1}: {match_percentage}%")
-        st.write("Summarized Resume:")
-        st.write(summarized_resume)
+        st.write(resume_text)
         st.write("-" * 50)
-
-    # Create a bar chart to display match percentages
-    chart_data = [match[0] for match in matches]
-    st.bar_chart(chart_data)
 
 st.caption(" ~ made by Team P7132")
